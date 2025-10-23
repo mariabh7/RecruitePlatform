07f501fd1976236f6cfbf8f6dfb432af208c9526
Maria <bouhrem.maria@gmail.com>
2025-10-23 20:35:36 +0100

    fix run issue

---
+++ src/app/(components)/Recruiters/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from "antd";
+import Link from "next/link";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="flex capitalize flex-col gap-10 justify-center items-center text-center">
+      hello
+    </div>
+  );
+};
+
+export default Page;
--- src/app/(components)/main/main.jsx
+++ src/app/(components)/main/main.jsx
@@ -7,12 +7,17 @@ const Main = () => {
     <div className="flex capitalize flex-col gap-10 justify-center items-center text-center">
       <p className="text-gray-500">are you ?</p>
       <div className="flex justify-start   gap-5 ">
-        <Button variant="solid" color="default" size="large">
-          <Link href={"/apply"}> an applicant</Link>
-        </Button>
-        <Button type="default" size="large" color="default" className="btn">
-          a recruiter
-        </Button>
+        <Link href={"/apply"}>
+          {" "}
+          <Button variant="solid" color="default" size="large">
+            an applicant
+          </Button>
+        </Link>
+        <Link href={"/recruiters"}>
+          <Button type="default" size="large" color="default" className="btn">
+            a recruiter
+          </Button>{" "}
+        </Link>
       </div>
     </div>
   );
